002de802172650c698df05fd075dad84ba100bae
artslob <aslob96@yandex.ru>
2017-12-25 22:33:52 +0300

    Lab 3: added script to copy files

---
+++ Lab3/copy_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PROJECT_ROOT="/mnt/e/Projects/Xilinx/Lab3"
+cp $PROJECT_ROOT/Lab2.srcs/sources_1/new/*.v ./
+cp $PROJECT_ROOT/Lab2.sdk/test_mb/src/main.c ./
+cp $PROJECT_ROOT/Lab2.srcs/sim_1/new/*.v ./
+
